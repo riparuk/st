1a7428d8bf68f6376356e2392152d7acb96c0dd5
riparuk <faruqirifa@gmail.com>
2023-06-25 02:17:11 +0700

    change font to source code pro

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Bitstream Vera Sans Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Source Code Pro:size=10";
 static int borderpx = 2;
 
 /*
