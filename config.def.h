3e0e7c71e7368fd3eb4c31ee0d12a6afdbe6da71
riparuk <faruqirifa@gmail.com>
2023-12-27 15:44:15 +0700

    Update Font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:size=10";
+static char *font =  "Source Code Pro:size=10:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
